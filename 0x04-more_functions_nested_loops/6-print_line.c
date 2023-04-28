8467a95458ba354c0261c3631bb2ff65e1f48320
Fatimaezzaharae sadouk <fatimaezzahraesadouk1996@gmail.com>
2023-04-28 05:56:07 -0700

    void print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- * print_time - Draws a straight line according to parametr
- * @n: The number of line to draw
+ * print_line - Draws a straight line according to parameter
+ * @n: The number of lines to draw
  * Return: empty
  */
 
-int print_line(int n)
+void print_line(int n)
 {
 	int x;
 
 	if (n <= 0)
 	{
-		_putchar(('\n');
-				}
-				else
-				{
-				for (x = 0; x < n; x++)
-				{
-				_putchar(95);
-				}
-				_putchar('\n');
-				}
-				}
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
