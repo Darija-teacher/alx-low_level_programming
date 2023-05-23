65f70660e1e70a3d63da9994d0baf3fcd20f468a
Fatimaezzaharae sadouk <fatimaezzahraesadouk1996@gmail.com>
2023-05-22 17:59:35 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,7 +7,7 @@
  * @age: Second member
  * @owner: Third member
  *
- * Descrition: Longer description
+ * Description: Longer description
  */
 struct dog
 {
@@ -22,7 +22,7 @@ struct dog
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print dog(struct dog *d);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
